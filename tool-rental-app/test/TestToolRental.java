dac3812aaa568443dac89af3709b1ba5538c03ed
Heather Sledd <hlsledd@gmail.com>
2024-07-23 21:09:56 -0600

    Restructures project under new module and asserts final charge values in tests.

--- tool-rental-app/test/TestToolRental.java
+++ tool-rental-app/test/TestToolRental.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +15,7 @@ public class TestToolRental {
     private final List<Tool> tools = new ArrayList<>();
 
     /**
-     * Initialize list of tools before conducting tests.
+     * Initializes list of tools before conducting tests.
      */
     @Before
     public void initializeTools() {
@@ -25,9 +26,9 @@ public class TestToolRental {
     }
 
     /**
-     * Assert that while attempting to call Tool#checkout with discount percentage of 101,
+     * Asserts while attempting to call Tool#checkout with discount percentage of 101,
      * an error is thrown as expected.
-     * Assert that the error message thrown is equivalent to the expected string regarding invalid discount percentage.
+     * Asserts that the error message thrown is equivalent to the expected string regarding invalid discount percentage.
      */
     @Test
     public void testInvalidDiscountPercent() {
@@ -39,38 +40,68 @@ public class TestToolRental {
         Assert.assertEquals("Discount percent must be between 0 and 100", thrown.getMessage());
     }
 
+    /**
+     * Asserts when checking out a Ladder from 7/2/20 for 3 days at a 10% discount
+     * the final charge is the expected value of $3.58
+     */
     @Test
-    public void test2() {
+    public void testLadderWithHolidayAndDiscount() {
         Tool tool = tools.get(1);
         RentalAgreement rentalAgreement = tool.checkout(LocalDate.of(2020, 7, 2), 3, 10);
         System.out.println(rentalAgreement);
+
+        Assert.assertEquals(rentalAgreement.calcFinalCharge(), new BigDecimal("3.58"));
     }
 
+    /**
+     * Asserts when checking out a Chainsaw from 7/2/15 for 5 days at a 25% discount
+     * the final charge is the expected value of $3.35
+     */
     @Test
-    public void test3() {
+    public void testChainsawWithHolidayAndDiscount() {
         Tool tool = tools.getFirst();
         RentalAgreement rentalAgreement = tool.checkout(LocalDate.of(2015, 7, 2), 5, 25);
         System.out.println(rentalAgreement);
+
+        Assert.assertEquals(rentalAgreement.calcFinalCharge(), new BigDecimal("3.35"));
     }
 
+    /**
+     * Asserts when checking out a Jackhammer (DeWalt) from 9/3/15 for 6 days at no discount
+     * the final charge is the expected value of $8.97
+     */
     @Test
-    public void test4() {
+    public void testJackhammerDWithHolidayNoDiscount() {
         Tool tool = tools.get(2);
         RentalAgreement rentalAgreement = tool.checkout(LocalDate.of(2015, 9, 3), 6, 0);
         System.out.println(rentalAgreement);
+
+        Assert.assertEquals(rentalAgreement.calcFinalCharge(), new BigDecimal("8.97"));
     }
 
+    /**
+     * Asserts when checking out a Jackhammer (Ridgid) from 7/2/15 for 9 days at no discount
+     * the final charge is the expected value of $17.94
+     */
     @Test
-    public void testJackhammerRWithHoliday() {
+    public void testJackhammerRWithHolidayNoDiscount() {
         Tool tool = tools.get(3);
         RentalAgreement rentalAgreement = tool.checkout(LocalDate.of(2015, 7, 2), 9, 0);
         System.out.println(rentalAgreement);
+
+        Assert.assertEquals(rentalAgreement.calcFinalCharge(), new BigDecimal("17.94"));
     }
 
+    /**
+     * Asserts when checking out a Jackhammer (Ridgid) from 7/2/20 for 4 days at a 50% discount
+     * the final charge is the expected value of $1.50
+     */
     @Test
     public void testJackhammerRWithHolidayAndDiscount() {
         Tool tool = tools.get(3);
         RentalAgreement rentalAgreement = tool.checkout(LocalDate.of(2020, 7, 2), 4, 50);
         System.out.println(rentalAgreement);
+
+        Assert.assertEquals(rentalAgreement.calcFinalCharge(), new BigDecimal("1.50"));
     }
 }
